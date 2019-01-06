c78870993e891e5adc712e11d903948e3f57891e
iphyer <mshen32@wisc.edu>
2019-01-05 23:04:25 -0600

    2019Plan

---
+++ _posts/2019-01-05-ReseachExperience1.markdown
@@ -0,0 +1,14 @@
+---
+layout: post
+title: "科研工作经验总结3"
+date: 2019-01-05 22:45
+comments: true
+categories: 科研
+---
+
+# 起因
+
+[毕业工作总结](https://iphyer.github.io/blog/2016/06/09/20160609summary/)
+[工作经验总结 2](https://iphyer.github.io/blog/2018/08/05/work/) 
+
+<!--more-->
---
+++ images/resize.sh
@@ -0,0 +1,25 @@
+#! /bin/bash 
+
+#获取脚本所在文件目录
+#大于1M图片缩小一半
+
+for file in  `find ./ -name "*.[jJ][pP][gG]"`;
+    do    	
+    	if [ `du -k "$file" | cut -f1` -gt 1000 ]
+		then
+    		echo $file
+    		mogrify -resize 50% $file
+		fi
+
+	done
+
+for file in  `find ./ -name "*.[pP][nN][gG]"`;
+    do 
+    	if [ `du -k "$file" | cut -f1` -gt 1000 ]
+		then
+    		echo $file
+    		mogrify -resize 50% $file
+		fi
+	done
+
+echo "Done"
